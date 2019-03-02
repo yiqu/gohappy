5162520b00bf2bd2a273dbb804db7ac40ad50430
Kevin Qu <kevinqu88@gmail.com>
2019-03-02 01:33:21 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModules } from './shared/material/material-barrel.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModules,
     AppRoutingModule
   ],
   providers: [],
